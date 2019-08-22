7eb8d426391b3dc10b5a727b0972505f86158c13
Ronaldo Lima <4389565+ronal2do@users.noreply.github.com>
2019-08-22 19:33:24 +0200

    all running

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+// import { HomeScreen as App} from '@boilerplate/ui'
+
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
